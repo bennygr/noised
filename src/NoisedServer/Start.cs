b2bd1c74eeb66bb07f36aa205d446d90527dc285
sebingel <sebingel+github@googlemail.com>
2015-04-29 15:47:30 +0200

    Code Cleanup
    -- Removed unused usings
    -- fixed mixed spacing/tabbing

--- src/NoisedServer/Start.cs
+++ src/NoisedServer/Start.cs
@@ -1,71 +1,67 @@
 using System;
-using System.Threading;
-using System.Collections.Generic;
 using System.Linq;
 using Noised.Core;
-using Noised.Logging;
 using Noised.Core.IOC;
-using Noised.Core.Plugins;
-using Noised.Core.Plugins.Audio;
-using Noised.Core.Plugins.Media;
-using Noised.Core.Commands;
-using Noised.Core.Service;
 using Noised.Core.Media;
+using Noised.Core.Plugins;
+using Noised.Core.Plugins.Media;
+using Noised.Core.Service;
+using Noised.Logging;
 
 namespace Noised.Server
 {
-	public class Start
-	{
-		public static int Main(String[] args)
-		{
-			IocContainer.Build();
+    public class Start
+    {
+        public static int Main(String[] args)
+        {
+            IocContainer.Build();
 
-			ILogging logger = IocContainer.Get<ILogging>();
-			logger.AddLogger(new ConsoleLogger());
+            ILogging logger = IocContainer.Get<ILogging>();
+            logger.AddLogger(new ConsoleLogger());
 
-			logger.Debug("Hello Noised");
-			IPluginLoader pluginLoader = IocContainer.Get<IPluginLoader>();
-			int pluginCount = pluginLoader.LoadPlugins("./plugins");
-			logger.Debug(pluginCount + " plugins loaded ");
+            logger.Debug("Hello Noised");
+            IPluginLoader pluginLoader = IocContainer.Get<IPluginLoader>();
+            int pluginCount = pluginLoader.LoadPlugins("./plugins");
+            logger.Debug(pluginCount + " plugins loaded ");
 
-			//Add a factory and create a ping command
-			ICore core = IocContainer.Get<ICore>();
-			core.Start();
+            //Add a factory and create a ping command
+            ICore core = IocContainer.Get<ICore>();
+            core.Start();
 
-			ServiceConnectionManager serviceConnectionManager = new ServiceConnectionManager();
-			serviceConnectionManager.StartServices();
+            ServiceConnectionManager serviceConnectionManager = new ServiceConnectionManager();
+            serviceConnectionManager.StartServices();
 
-			IMediaSource mediaSource = pluginLoader.GetPlugin<IMediaSource>();
-			if(mediaSource != null)
-			{
-
-
-				try
-				{
-					var resultList = mediaSource.Search("test");
-					MediaItem test = resultList.First();
-					Console.WriteLine(test.Protocol);
-					IocContainer.Get<IMediaManager>().Play(test);
-				}
-				catch(Exception e)
-				{
-					logger.Error(e.Message);
-				}
+            IMediaSource mediaSource = pluginLoader.GetPlugin<IMediaSource>();
+            if (mediaSource != null)
+            {
 
 
-				//IAudioPlugin audioPlugin = 
-				//	pluginLoader.GetPlugin<IAudioPlugin>();
-				//audioPlugin.SongFinished += 
-				//	(sender,mediaItem) => 
-				//	{ 
-				//		Console.WriteLine("SONG HAS BEEN FINISHED. I WANT MORE MUSIC :-)"); 
-				//	};
-			}
-			else
-			{
-					logger.Error("No media source plugin found");
-			}
-			return 0;
-		}
-	};
+                try
+                {
+                    var resultList = mediaSource.Search("test");
+                    MediaItem test = resultList.First();
+                    Console.WriteLine(test.Protocol);
+                    IocContainer.Get<IMediaManager>().Play(test);
+                }
+                catch (Exception e)
+                {
+                    logger.Error(e.Message);
+                }
+
+
+                //IAudioPlugin audioPlugin = 
+                //	pluginLoader.GetPlugin<IAudioPlugin>();
+                //audioPlugin.SongFinished += 
+                //	(sender,mediaItem) => 
+                //	{ 
+                //		Console.WriteLine("SONG HAS BEEN FINISHED. I WANT MORE MUSIC :-)"); 
+                //	};
+            }
+            else
+            {
+                logger.Error("No media source plugin found");
+            }
+            return 0;
+        }
+    };
 }
