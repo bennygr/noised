14ca10e7e7cee485607739dec58c12b8394bf88f
Benny <bennygr@gmx.net>
2015-05-11 20:47:33 +0200

    * Added first unit test project with a simple test

---
+++ src/NoisedTests/Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using Should;
+
+namespace NoisedTests
+{
+	[TestFixture]
+	public class Test
+	{
+		[Test]
+		public void ShouldNotFail()
+		{
+			true.ShouldBeTrue();	
+		}
+	}
+}
