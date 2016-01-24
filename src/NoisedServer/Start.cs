a6dcd47a6c9f5bfb5f96397f57d325a9d64ea267
sebingel <sebingel+github@googlemail.com>
2016-01-24 17:28:14 +0100

    - sdded reference to CoreCommands to NoisedServer -- This is just for easier testing of commands and should be removed after development

--- src/NoisedServer/Start.cs
+++ src/NoisedServer/Start.cs
@@ -6,6 +6,7 @@ using Noised.Core.Media;
 using Noised.Core.Plugins;
 using Noised.Core.Service;
 using Noised.Logging;
+using Noised.Plugins.Commands.CoreCommands;
 
 namespace Noised.Server
 {
@@ -17,26 +18,26 @@ namespace Noised.Server
             ILogging logger = IocContainer.Get<ILogging>();
             logger.AddLogger(new ConsoleLogger());
             logger.Info("Hello I am Noised - your friendly music player daemon");
-			
+
             //Creating the DB or update if neccessary
             var db = IocContainer.Get<IDB>();
             db.CreateOrUpdate();
-			
+
             //installing new plugins
             var pluginInstaller = IocContainer.Get<IPluginInstaller>();
             pluginInstaller.InstallAll("./plugins");
-			
+
             //Loading configuration
             var config = IocContainer.Get<IConfig>();
             config.Load(IocContainer.Get<IConfigurationLoader>());
 
             //loading plugins
-			logger.Info("Loading plugins:");
+            logger.Info("Loading plugins:");
             var pluginLoader = IocContainer.Get<IPluginLoader>();
             int pluginCount = pluginLoader.LoadPlugins();
             logger.Info(pluginCount + " plugins loaded ");
 
-            //Add a factory and create a ping command
+            //Add a factory and create a ping command ???
             var core = IocContainer.Get<ICore>();
             core.Start();
 
@@ -44,13 +45,17 @@ namespace Noised.Server
             var serviceConnectionManager = new ServiceConnectionManager();
             serviceConnectionManager.StartServices();
 
-			// Refreshin music
+            // Refreshing music
             logger.Info("Refreshing music...");
             var sourceAccumulator = IocContainer.Get<IMediaSourceAccumulator>();
             sourceAccumulator.Refresh();
             logger.Info("Done refreshing music.");
-			
+
             logger.Info("Noised has been started.");
+
+            // ---------- Testcode ----------
+            core.ExecuteCommandAsync(new Play(null, @"C:\test\test.mp3"));
+
             return 0;
         }
     }
