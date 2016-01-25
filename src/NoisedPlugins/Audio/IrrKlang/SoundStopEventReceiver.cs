cb6d9c81e8f8cb07ed8006382f9d31e5b4725915
sebingel <sebingel+github@googlemail.com>
2016-01-25 23:41:20 +0100

    - Added SoundStopEventReceiver to track if IrrKlang plugin has finished playing a song

---
+++ src/NoisedPlugins/Audio/IrrKlang/SoundStopEventReceiver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using IrrKlang;
+using Noised.Core.Media;
+using Noised.Core.Plugins.Audio;
+
+namespace Noised.Plugins.Audio.IrrKlang
+{
+    public class SoundStopEventReceiver : ISoundStopEventReceiver
+    {
+        private readonly Action<AudioEventArgs> invokeOnSongFinished;
+
+        public SoundStopEventReceiver(Action<AudioEventArgs> invokeOnSongFinished)
+        {
+            this.invokeOnSongFinished = invokeOnSongFinished;
+        }
+
+        #region Implementation of ISoundStopEventReceiver
+
+        public void OnSoundStopped(ISound sound, StopEventCause reason, object userData)
+        {
+            invokeOnSongFinished(new AudioEventArgs { MediaItem = (MediaItem)userData });
+        }
+
+        #endregion
+    }
+}
