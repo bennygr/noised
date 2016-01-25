6a48bddaac1c9e9c6f9708169e2f8c7ba6eb5923
sebingel <sebingel+github@googlemail.com>
2016-01-25 23:36:52 +0100

    - added Playlist.GetNextItem - Code Cleanup

--- src/NoisedCore/Media/Playlist.cs
+++ src/NoisedCore/Media/Playlist.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 
 namespace Noised.Core.Media
 {
     public class Playlist
     {
         private readonly List<MediaItem> items;
+
+        private MediaItem currentMediaItem;
 
         public string Name { get; private set; }
 
@@ -31,6 +34,17 @@ namespace Noised.Core.Media
         public void Add(MediaItem item)
         {
             items.Add(item);
+        }
+
+        public MediaItem GetNextItem()
+        {
+            if (currentMediaItem == null)
+            {
+                currentMediaItem = items.First();
+                return currentMediaItem;
+            }
+
+            return items.ElementAtOrDefault(items.IndexOf(currentMediaItem));
         }
     }
 }
