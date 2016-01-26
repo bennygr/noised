f37b799d85ebb07eea45e2d786f2a43c2221c70e
sebingel <sebingel+github@googlemail.com>
2016-01-26 23:48:36 +0100

    - Added a command to create a Playlist - Added a command to add MediaItems to a Playlist

---
+++ src/NoisedPlugins/Commands/CoreCommands/AddToPlaylist.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class AddToPlaylist : AbstractCommand
+    {
+        private readonly string playlistName;
+        private readonly IEnumerable<string> mediaItemUris;
+
+        ///  <summary>
+        /// 		Constructor
+        ///  </summary>
+        ///  <param name="context">The command's context</param>
+        /// <param name="playlistName">Name of the Playlist to which the Items should be added</param>
+        /// <param name="mediaItemUris">List of Uris of MediaItems that should be added</param>
+        public AddToPlaylist(ServiceConnectionContext context, string playlistName, IList<string> mediaItemUris)
+            : base(context)
+        {
+            this.playlistName = playlistName;
+            this.mediaItemUris = mediaItemUris;
+            if (context == null)
+                throw new ArgumentNullException("context");
+            if (String.IsNullOrWhiteSpace(playlistName))
+                throw new ArgumentException("please provide a valid Playlist name", "playlistName");
+            if (mediaItemUris == null || !mediaItemUris.Any())
+                throw new ArgumentNullException("mediaItemUris");
+        }
+
+        #region Overrides of AbstractCommand
+
+        /// <summary>
+        ///		Defines the command's behaviour
+        /// </summary>
+        protected override void Execute()
+        {
+            foreach (string mediaItemUri in mediaItemUris)
+            {
+                IocContainer.Get<IPlaylistManager>()
+                    .FindPlaylists(playlistName)
+                    .Add(IocContainer.Get<IMediaSourceAccumulator>().Get(new Uri(mediaItemUri)));
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/NoisedPlugins/Commands/CoreCommands/CreatePlaylist.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class CreatePlaylist : AbstractCommand
+    {
+        private readonly string name;
+
+        ///  <summary>
+        /// 		Constructor
+        ///  </summary>
+        ///  <param name="context">The command's context</param>
+        /// <param name="name">Name of the playlist</param>
+        public CreatePlaylist(ServiceConnectionContext context, string name)
+            : base(context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+
+            if (String.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("please provide a valid Playlist name", "name");
+
+            this.name = name;
+        }
+
+        #region Overrides of AbstractCommand
+
+        /// <summary>
+        ///		Defines the command's behaviour
+        /// </summary>
+        protected override void Execute()
+        {
+            IocContainer.Get<IPlaylistManager>().AddPlaylist(IocContainer.Get<IPlaylistManager>().CreatePlaylist(name));
+        }
+
+        #endregion
+    }
+}
