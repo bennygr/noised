0d8c455609ef9862fefa112f7538793b5ace4fa4
sebingel <sebingel+github@googlemail.com>
2016-01-30 15:35:48 +0100

    - Code Celanup

--- src/NoisedCore/DB/Sqlite/SqliteDB.cs
+++ src/NoisedCore/DB/Sqlite/SqliteDB.cs
@@ -1,26 +1,26 @@
 using System.Collections.Generic;
-using Noised.Core.DB;
 
 namespace Noised.Core.DB.Sqlite
 {
-	public class SqliteDB : IDB
-	{
-	    private List<string> GenerateCreateStatements()
-	    {
-			return new List<string>
-			{
-				PluginsSql.CREATE_TABLE_STMT,
-				PluginFilesSql.CREATE_TABLE_STMT
-			};	
-	    }
-	
-	    #region IDB implementation
-	
-	    public void CreateOrUpdate()
-	    {
-			new SqliteDBCreator().CreateOrUpdate(new SqliteCoreConnectionFactory(),GenerateCreateStatements());
-	    }
-	
-	    #endregion
-	};
+    public class SqliteDB : IDB
+    {
+        private static List<string> GenerateCreateStatements()
+        {
+            return new List<string>
+            {
+                PluginsSql.CREATE_TABLE_STMT,
+                PluginFilesSql.CREATE_TABLE_STMT,
+                PlaylistSql.CreateTableStmt
+            };
+        }
+
+        #region IDB implementation
+
+        public void CreateOrUpdate()
+        {
+            new SqliteDBCreator().CreateOrUpdate(new SqliteCoreConnectionFactory(), GenerateCreateStatements());
+        }
+
+        #endregion
+    };
 }
