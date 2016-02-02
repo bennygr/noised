86288223f15c08cc610b45988d5a135a9679f234
sebingel <sebingel+github@googlemail.com>
2016-02-02 23:10:22 +0100

    - added sqlite3.dll as a resource in Noised.Core - added a ResourceExtractor class to extract resources to the filesystem

---
+++ src/NoisedCore/Resources/ResourceExtractor.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Noised.Core.IOC;
+using Noised.Logging;
+
+namespace Noised.Core.Resources
+{
+    public static class ResourceExtractor
+    {
+        /// <summary>
+        /// Extracts Embedded Resources to a certain location
+        /// </summary>
+        /// <param name="outputDir">Destination Path</param>
+        /// <param name="resourceLocation">Location of Resources in the Assembly</param>
+        /// <param name="files">List of Names of Files to extract</param>
+        public static void ExtractEmbeddedResource(string outputDir, string resourceLocation, List<string> files)
+        {
+            ILogging log = IocContainer.Get<ILogging>();
+
+            foreach(string file in files)
+            {
+                log.Debug("Extracting of " + file + " ...");
+
+                using(Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceLocation + @"." + file))
+                {
+                    string path = Path.Combine(outputDir, file);
+                    log.Debug("... into " + path);
+
+                    if(File.Exists(path))
+                    {
+                        log.Debug(path + " already exists. Skipping extraction.");
+                        continue;
+                    }
+
+                    using(FileStream fileStream = new FileStream(path, FileMode.Create))
+                    {
+                        for(int i = 0; i < stream.Length; i++)
+                            fileStream.WriteByte((byte)stream.ReadByte());
+                        fileStream.Close();
+                        log.Debug("Extraction completed.");
+                    }
+                }
+            }
+        }
+    }
+}
