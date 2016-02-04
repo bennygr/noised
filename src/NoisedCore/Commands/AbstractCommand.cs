db1735a6228a94063873bc87a6cb89a990a4db73
sebingel <sebingel+github@googlemail.com>
2016-02-04 13:02:59 +0100

    - Code Cleanup

--- src/NoisedCore/Commands/AbstractCommand.cs
+++ src/NoisedCore/Commands/AbstractCommand.cs
@@ -1,61 +1,60 @@
-using System;
-using Noised.Core.IOC;
 using Noised.Core.Service;
+
 namespace Noised.Core.Commands
 {
-	/// <summary>
-	///		An abstract noised command
-	/// </summary>
-	/// <remarks>
-	///		Extend this class in order to define your own commands
-	/// </remarks>
-	public abstract class AbstractCommand
-	{
-		#region Properties
-		
-		/// <summary>
-		///		The contect in which the command is executed
-		/// </summary>
-		public ServiceConnectionContext Context{get; private set;}
+    /// <summary>
+    ///		An abstract noised command
+    /// </summary>
+    /// <remarks>
+    ///		Extend this class in order to define your own commands
+    /// </remarks>
+    public abstract class AbstractCommand
+    {
+        #region Properties
 
-		/// <summary>
-		///		Whether the command requires authentication or not
-		/// </summary>
-		public bool RequiresAuthentication{get;protected set;}
+        /// <summary>
+        ///		The contect in which the command is executed
+        /// </summary>
+        public ServiceConnectionContext Context { get; private set; }
 
-		#endregion
+        /// <summary>
+        ///		Whether the command requires authentication or not
+        /// </summary>
+        public bool RequiresAuthentication { get; protected set; }
 
-		#region Constructor
-	
-		/// <summary>
-		///		Constructor
-		/// </summary>
-		/// <param name="context">The command's context</param>
-		/// <param name="requiresAuthentication">Whether the command requires authentication or not</param>
-		protected AbstractCommand (ServiceConnectionContext context,
-								   bool requiresAuthentication = true)
-		{
-			this.Context = context;
-			this.RequiresAuthentication = requiresAuthentication;
-		}
-	
-		#endregion
+        #endregion
 
-		#region Methods
+        #region Constructor
 
-		/// <summary>
-		///		Defines the command's behaviour
-		/// </summary>
-		protected abstract void Execute();
-		
-		/// <summary>
-		///		Executes the command
-		/// </summary>
-		public void ExecuteCommand()
-		{
-			Execute();
-		}
-		
-		#endregion
-	};
+        /// <summary>
+        ///		Constructor
+        /// </summary>
+        /// <param name="context">The command's context</param>
+        /// <param name="requiresAuthentication">Whether the command requires authentication or not</param>
+        protected AbstractCommand(ServiceConnectionContext context,
+            bool requiresAuthentication = true)
+        {
+            Context = context;
+            RequiresAuthentication = requiresAuthentication;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        ///		Defines the command's behaviour
+        /// </summary>
+        protected abstract void Execute();
+
+        /// <summary>
+        ///		Executes the command
+        /// </summary>
+        public void ExecuteCommand()
+        {
+            Execute();
+        }
+
+        #endregion
+    };
 }
