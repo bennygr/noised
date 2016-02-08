1e2d7dedc0ea35b43168f618ed70506dbe72bc2a
sebingel <sebingel+github@googlemail.com>
2016-02-08 11:47:25 +0100

    - Added MediaItemNotFoundException

---
+++ src/NoisedCore/Media/MediaItemNotFoundException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Noised.Core.Media
+{
+    public class MediaItemNotFoundException : Exception
+    {
+        public MediaItemNotFoundException(string message)
+            : base(message)
+        { }
+
+        public MediaItemNotFoundException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+    }
+}
