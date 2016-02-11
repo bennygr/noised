0fc912cd0cfcd95faf52da92d5faef55899eef8b
sebingel <sebingel+github@googlemail.com>
2016-02-11 13:01:18 +0100

    - First steps into UnitTesting the Playlist Code

--- src/NoisedTests/Media/PlaylistTests.cs
+++ src/NoisedTests/Media/PlaylistTests.cs
@@ -1,4 +1,7 @@
-﻿using Noised.Core.IOC;
+﻿using System.Collections.Generic;
+using Moq;
+using Noised.Core.DB;
+using Noised.Core.IOC;
 using Noised.Core.Media;
 using NUnit.Framework;
 using Should;
@@ -9,12 +12,25 @@ namespace NoisedTests.Media
     public class PlaylistTests
     {
         private IPlaylistManager pman;
-        
+
         [SetUp]
         public void PlaylistTestsSetup()
         {
             IocContainer.Build();
             pman = IocContainer.Get<IPlaylistManager>();
+
+            Mock<IDbFactory> mockFactory = new Mock<IDbFactory>();
+            Mock<IUnitOfWork> mockUnitOfWork = new Mock<IUnitOfWork>();
+            Mock<IPlaylistRepository> mockPlaylistRepository = new Mock<IPlaylistRepository>();
+
+            mockPlaylistRepository.Setup(x => x.GetAllPlaylists()).Returns(new List<Playlist>());
+
+            mockUnitOfWork.Setup(x => x.PlaylistRepository).Returns(mockPlaylistRepository.Object);
+
+            mockFactory.Setup(x => x.GetUnitOfWork()).Returns(mockUnitOfWork.Object);
+
+            pman.DbFactory = mockFactory.Object;
+            pman.RefreshPlaylists();
         }
 
         [Test]
