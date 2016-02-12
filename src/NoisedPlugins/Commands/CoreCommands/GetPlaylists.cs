832dda9530d31cdec889c5ad4468b40db556728c
sebingel <sebingel+github@googlemail.com>
2016-02-12 12:08:09 +0100

    - Added Command GetPlaylists

---
+++ src/NoisedPlugins/Commands/CoreCommands/GetPlaylists.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class GetPlaylists : AbstractCommand
+    {
+        /// <summary>
+        ///		Constructor
+        /// </summary>
+        /// <param name="context">The command's context</param>
+        public GetPlaylists(ServiceConnectionContext context)
+            : base(context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+        }
+
+        #region Overrides of AbstractCommand
+
+        /// <summary>
+        ///		Defines the command's behaviour
+        /// </summary>
+        protected override void Execute()
+        {
+            Context.SendResponse(new ResponseMetaData
+            {
+                Name = "Noised.Plugins.Commands.CoreCommands.GetPlaylists",
+                Parameters = new List<object>(IocContainer.Get<IPlaylistManager>().Playlists)
+            });
+        }
+
+        #endregion
+    }
+}
