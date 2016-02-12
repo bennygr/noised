3b2d52385c1084a1bf008f4e6915234e2033f51a
sebingel <sebingel+github@googlemail.com>
2016-02-12 21:12:09 +0100

    - Added UnitTests for ShuffleStatus Commands

---
+++ src/NoisedTests/Commands/ShuffleStatusTest.cs
@@ -0,0 +1,37 @@
+﻿using Moq;
+using Noised.Core;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+using Noised.Plugins.Commands.CoreCommands;
+using NUnit.Framework;
+using Should;
+
+namespace NoisedTests.Commands
+{
+    [TestFixture]
+    public class ShuffleStatusTest
+    {
+        [SetUp]
+        public void ShuffleStatusTestSetUp()
+        {
+            IocContainer.Build();
+        }
+
+        [Test]
+        public void SetShuffleStatusTest()
+        {
+            IocContainer.Get<IMediaManager>().Shuffle.ShouldBeFalse();
+            
+            IocContainer.Get<ICore>().ExecuteCommand(new SetShuffleStatus(new Mock<IServiceConnectionContext>().Object, true));
+
+            IocContainer.Get<IMediaManager>().Shuffle.ShouldBeTrue();
+        }
+
+        [Test]
+        public void GetShuffleStatusTest()
+        {
+            
+        }
+    }
+}
