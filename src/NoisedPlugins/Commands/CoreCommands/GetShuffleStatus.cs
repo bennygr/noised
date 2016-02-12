43e2121887f60f6f7fa100ac1e2ce0ad49de9cab
sebingel <sebingel+github@googlemail.com>
2016-02-12 21:11:18 +0100

    - Added Commands GetShuffleStatus and SetShuffleStatus

---
+++ src/NoisedPlugins/Commands/CoreCommands/GetShuffleStatus.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+  public  class GetShuffleStatus : AbstractCommand
+    {
+        public GetShuffleStatus(IServiceConnectionContext context):base(context)
+        { }
+
+        #region Overrides of AbstractCommand
+
+        protected override void Execute()
+        {
+            Context.SendResponse(new ResponseMetaData
+            {
+                Name = "Noised.Plugins.Commands.CoreCommands.GetShuffleStatus",
+                Parameters = new List<object> { IocContainer.Get<IMediaManager>().Shuffle }
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ src/NoisedPlugins/Commands/CoreCommands/SetShuffleStatus.cs
@@ -0,0 +1,26 @@
+﻿using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class SetShuffleStatus : AbstractCommand
+    {
+        private bool shuffleStatus;
+
+        public SetShuffleStatus(IServiceConnectionContext context, bool shuffleStatus):base(context)
+        {
+            this.shuffleStatus = shuffleStatus;
+        }
+
+        #region Overrides of AbstractCommand
+
+        protected override void Execute()
+        {
+            IocContainer.Get<IMediaManager>().Shuffle = shuffleStatus;
+        }
+
+        #endregion
+    }
+}
