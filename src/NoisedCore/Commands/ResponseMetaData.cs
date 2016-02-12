8ca9d54a5192c1f8c193efd377f15b888cd51bf2
sebingel <sebingel+github@googlemail.com>
2016-02-12 12:07:32 +0100

    - Code Cleanup

--- src/NoisedCore/Commands/ResponseMetaData.cs
+++ src/NoisedCore/Commands/ResponseMetaData.cs
@@ -3,34 +3,34 @@ using System.Collections.Generic;
 
 namespace Noised.Core.Commands
 {
-	/// <summary>
-	///		Meta data for outgoing responses
-	/// </summary>
-	public class ResponseMetaData
-	{
-		public ResponseMetaData()
-		{
-			ProtocolVersion = 1.0f;
-		}
+    /// <summary>
+    ///		Meta data for outgoing responses
+    /// </summary>
+    public class ResponseMetaData
+    {
+        public ResponseMetaData()
+        {
+            ProtocolVersion = 1.0f;
+        }
 
-		/// <summary>
-		///		The version of the protocol
-		/// </summary>
-		public float ProtocolVersion{get;private set;}
+        /// <summary>
+        ///		The version of the protocol
+        /// </summary>
+        public float ProtocolVersion { get; private set; }
 
-		/// <summary>
-		///		The name of the response
-		/// </summary>
-		public String Name{get;set;}
+        /// <summary>
+        ///		The name of the response
+        /// </summary>
+        public String Name { get; set; }
 
-		/// <summary>
-		///		Parameters of the response
-		/// </summary>
-		public List<Object> Parameters{get;set;}
+        /// <summary>
+        ///		Parameters of the response
+        /// </summary>
+        public List<Object> Parameters { get; set; }
 
-		public override string ToString() 
-		{
-			return Name;
-		}
-	};
+        public override string ToString()
+        {
+            return Name;
+        }
+    };
 }
