254e69ce36d6b4ea20f67577fbabdd9d8205d735
sebingel <sebingel+github@googlemail.com>
2016-02-12 21:09:09 +0100

    - Added Interface IServiceConnectionContext

---
+++ src/NoisedCore/Service/IServiceConnectionContext.cs
@@ -0,0 +1,15 @@
+﻿using Noised.Core.Commands;
+using Noised.Logging;
+
+namespace Noised.Core.Service
+{
+    public interface IServiceConnectionContext
+    {
+        IServiceConnection Connection { get; }
+        bool IsAuthenticated { get; set; }
+        ILogging Logging { get; }
+        User User { get; set; }
+
+        void SendResponse(ResponseMetaData response);
+    }
+}
