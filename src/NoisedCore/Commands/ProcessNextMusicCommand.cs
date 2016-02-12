e263a4a52dd10f89d5c69f8438b4d4818b8f6ee1
sebingel <sebingel+github@googlemail.com>
2016-02-12 12:37:13 +0100

    - ProcessNextMusicCommand now checks the current Playlist after checking the Queue

--- src/NoisedCore/Commands/ProcessNextMusicCommand.cs
+++ src/NoisedCore/Commands/ProcessNextMusicCommand.cs
@@ -1,4 +1,3 @@
-using Noised.Core.Commands;
 using Noised.Core.IOC;
 using Noised.Core.Media;
 
@@ -8,29 +7,41 @@ namespace Noised.Core.Commands
     {
         internal ProcessNextMusicCommand()
             : base(null)
-        {
-        }
+        { }
 
         #region implemented abstract members of AbstractCommand
 
         protected override void Execute()
         {
-			var mediaManager = IocContainer.Get<IMediaManager>();
+            var mediaManager = IocContainer.Get<IMediaManager>();
             var queue = IocContainer.Get<IQueue>();
-			//Checking queue for new music 
-			//TODO: look at the current playlist
+            //Checking queue for new music 
             Listable<MediaItem> nextItem = queue.Dequeue();
+
+            // If nothing was queued we check if a Playlist is loaded and if it can give us new music
+            if (nextItem == null)
+            {
+                IPlaylistManager pman = IocContainer.Get<IPlaylistManager>();
+
+                if (pman != null)
+                {
+                    Playlist p = pman.LoadedPlaylist;
+
+                    if (p != null)
+                        nextItem = p.NextItem;
+                }
+            }
+
             if (nextItem != null)
             {
                 mediaManager.Play(nextItem.Item);
             }
-			else
-			{
+            else
+            {
                 mediaManager.Stop();
-			}
+            }
         }
 
         #endregion
-	
     };
 }
