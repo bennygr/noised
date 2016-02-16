acece233d51642294ef202d86e3fdeabee81759d
sebingel <sebingel+github@googlemail.com>
2016-02-16 12:47:40 +0100

    - Added GetRepeatMode command - Added SetRepeatMode command

---
+++ src/NoisedPlugins/Commands/CoreCommands/GetRepeatMode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class GetRepeatMode : AbstractCommand
+    {
+        /// <summary>
+        ///	Constructor
+        /// </summary>
+        /// <param name="context">The command's context</param>
+        public GetRepeatMode(IServiceConnectionContext context)
+            : base(context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+        }
+
+        #region Overrides of AbstractCommand
+
+        /// <summary>
+        ///	Defines the command's behaviour
+        /// </summary>
+        protected override void Execute()
+        {
+            Context.SendResponse(new ResponseMetaData
+            {
+                Name = "Noised.Plugins.Commands.CoreCommands.GetRepeatMode",
+                Parameters = new List<object> { IocContainer.Get<IMediaManager>().Repeat }
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ src/NoisedPlugins/Commands/CoreCommands/SetRepeatMode.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class SetRepeatMode : AbstractCommand
+    {
+        private RepeatMode rmRepeatMode;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context">The command's context</param>
+        /// <param name="repeatMode"></param>
+        public SetRepeatMode(IServiceConnectionContext context, string repeatMode)
+            : base(context)
+        {
+            if (context == null)
+                throw new ArgumentNullException("context");
+            if (String.IsNullOrWhiteSpace(repeatMode))
+                throw new ArgumentNullException("repeatMode");
+
+            rmRepeatMode = (RepeatMode)Enum.Parse(typeof(RepeatMode), repeatMode, true);
+        }
+
+        #region Overrides of AbstractCommand
+
+        /// <summary>
+        /// Defines the command's behaviour
+        /// </summary>
+        protected override void Execute()
+        {
+            IocContainer.Get<IMediaManager>().Repeat = rmRepeatMode;
+        }
+
+        #endregion
+    }
+}
