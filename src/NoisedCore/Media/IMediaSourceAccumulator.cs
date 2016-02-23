63f76f85f2c3dac2d4644e8c13aad79cf6b6d633
sebingel <sebingel+github@googlemail.com>
2016-02-23 12:34:30 +0100

    - Added Property Sources to IMediaSourceAccumulator and implemented it in MediaSourceAccumulator - Added Method Search with source selection to IMediaSourceAccumulator and implemented it in MediaSourceAccumulator

--- src/NoisedCore/Media/IMediaSourceAccumulator.cs
+++ src/NoisedCore/Media/IMediaSourceAccumulator.cs
@@ -3,29 +3,44 @@ using System.Collections.Generic;
 
 namespace Noised.Core.Media
 {
-	/// <summary>
-	///		Provides access to all known IMedaSource 
-	/// </summary>
-	public interface IMediaSourceAccumulator
-	{
-		/// <summary>
-		///		Refreshs all known MediaSources
-		/// </summary>
-		void Refresh();
+    /// <summary>
+    ///		Provides access to all known IMedaSource 
+    /// </summary>
+    public interface IMediaSourceAccumulator
+    {
+        /// <summary>
+        /// Refreshs all known MediaSources
+        /// </summary>
+        void Refresh();
 
-		/// <summary>
-		///		Searches all known IMediaSources for the given URI
-		/// </summary>
-		/// <returns> The first found MediaItem for the given URI, or null if no such item was found </returns>
-		MediaItem Get(Uri uri);
-	
-		/// <summary>
-		///		Searches all known IMediaSource's for the given search pattern
-		/// </summary>
-		/// <param name="pattern">The search pattern</param>
-		/// <returns>
-		///		An enumeration of search results matching the given pattern
-		/// </returns>
-		IEnumerable<MediaSourceSearchResult> Search(string pattern);
-	};
+        /// <summary>
+        /// Searches all known IMediaSources for the given URI
+        /// </summary>
+        /// <returns> The first found MediaItem for the given URI, or null if no such item was found </returns>
+        MediaItem Get(Uri uri);
+
+        /// <summary>
+        /// Searches all known IMediaSources for the given search pattern
+        /// </summary>
+        /// <param name="pattern">The search pattern</param>
+        /// <returns>
+        /// An enumeration of search results matching the given pattern
+        /// </returns>
+        IEnumerable<MediaSourceSearchResult> Search(string pattern);
+
+        /// <summary>
+        /// Searches a set of IMediaSources for the given search pattern
+        /// </summary>
+        /// <param name="pattern">The search pattern</param>
+        /// <param name="sourceIdentifiers">The identifiers of the IMediaSources to search in</param>
+        /// <returns>
+        /// An enumeration of search results matching the given pattern
+        /// </returns>
+        IEnumerable<MediaSourceSearchResult> Search(string pattern, IEnumerable<string> sourceIdentifiers);
+
+        /// <summary>
+        /// Gets all Sources
+        /// </summary>
+        IEnumerable<string> Sources { get; }
+    };
 }
--- src/NoisedCore/Media/MediaSourceAccumulator.cs
+++ src/NoisedCore/Media/MediaSourceAccumulator.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Noised.Core.IOC;
 using Noised.Core.Plugins;
 using Noised.Core.Plugins.Media;
@@ -27,14 +28,14 @@ namespace Noised.Core.Media
             foreach (IMediaSource mediaSource in pluginLoader.GetPlugins<IMediaSource>())
             {
                 IocContainer.Get<ILogging>().Info(String.Format("Refreshing media source {0}...",
-                                                                mediaSource.Identifier));
+                    mediaSource.Identifier));
                 DateTime t0 = DateTime.Now;
                 mediaSource.Refresh();
                 DateTime t1 = DateTime.Now;
                 TimeSpan td = t1 - t0;
                 IocContainer.Get<ILogging>().Info(String.Format("Refreshed media source {0} in {1} seconds",
-                                                                mediaSource.Identifier,
-                                                                td.TotalSeconds));
+                    mediaSource.Identifier,
+                    td.TotalSeconds));
             }
         }
 
@@ -44,21 +45,57 @@ namespace Noised.Core.Media
             {
                 var ret = source.Get(uri);
                 if (ret != null)
-                {
                     return ret;
-                }
             }
             return null;
         }
 
+        /// <summary>
+        /// Searches all known IMediaSources for the given search pattern
+        /// </summary>
+        /// <param name="pattern">The search pattern</param>
+        /// <returns>
+        /// An enumeration of search results matching the given pattern
+        /// </returns>
         public IEnumerable<MediaSourceSearchResult> Search(string pattern)
         {
-            var ret = new List<MediaSourceSearchResult>();
-            foreach (var source in pluginLoader.GetPlugins<IMediaSource>())
+            return Search(pattern, null);
+        }
+
+        /// <summary>
+        /// Searches a set of IMediaSources for the given search pattern
+        /// </summary>
+        /// <param name="pattern">The search pattern</param>
+        /// <param name="sourceIdentifiers">The identifiers of the IMediaSources to search in</param>
+        /// <returns>
+        /// An enumeration of search results matching the given pattern
+        /// </returns>
+        public IEnumerable<MediaSourceSearchResult> Search(string pattern, IEnumerable<string> sourceIdentifiers)
+        {
+            List<MediaSourceSearchResult> ret = new List<MediaSourceSearchResult>();
+
+            foreach (IMediaSource source in pluginLoader.GetPlugins<IMediaSource>())
             {
+                if (sourceIdentifiers != null &&
+                    sourceIdentifiers.Any() &&
+                    !sourceIdentifiers.Contains(source.Identifier))
+                    continue;
+
                 ret.Add(source.Search(pattern));
             }
+
             return ret;
+        }
+
+        /// <summary>
+        /// Gets all Sources
+        /// </summary>
+        public IEnumerable<string> Sources
+        {
+            get
+            {
+                return pluginLoader.GetPlugins<IMediaSource>().Select(mediaSource => mediaSource.Identifier);
+            }
         }
 
         #endregion
