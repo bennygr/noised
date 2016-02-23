a823d3484633c81dac4fee2ba1565629eb1664ea
sebingel <sebingel+github@googlemail.com>
2016-02-23 12:35:26 +0100

    - Added Command GetMediaSources to get all MediaSources

---
+++ src/NoisedPlugins/Commands/CoreCommands/GetMediaSources.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Noised.Core.Commands;
+using Noised.Core.IOC;
+using Noised.Core.Media;
+using Noised.Core.Service;
+
+namespace Noised.Plugins.Commands.CoreCommands
+{
+    public class GetMediaSources : AbstractCommand
+    {
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context">The command's context</param>
+        public GetMediaSources(IServiceConnectionContext context)
+            : base(context)
+        { }
+
+        #region Overrides of AbstractCommand
+
+        /// <summary>
+        /// Defines the command's behaviour
+        /// </summary>
+        protected override void Execute()
+        {
+            Context.SendResponse(new ResponseMetaData
+            {
+                Name = "Noised.Commands.Core.GetMediaSources",
+                Parameters = new List<object>(IocContainer.Get<IMediaSourceAccumulator>().Sources)
+            });
+        }
+
+        #endregion
+    }
+}
