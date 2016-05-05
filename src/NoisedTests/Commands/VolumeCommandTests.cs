81c5bec2ed0917270a7c565c9f71d643bf097f3d
Benny <bennygr@gmx.net>
2016-05-05 14:45:38 +0200

    Introduced AbstractCommandTest in order to allow easier unit testing of noised commands

--- src/NoisedTests/Commands/VolumeCommandTests.cs
+++ src/NoisedTests/Commands/VolumeCommandTests.cs
@@ -1,58 +1,35 @@
-﻿//using Moq;
-//using Noised.Core;
-//using Noised.Core.Commands;
-//using Noised.Core.IOC;
-//using Noised.Core.Media;
-//using Noised.Core.Service;
-//using Noised.Plugins.Commands.CoreCommands;
-//using NUnit.Framework;
-//using Should;
-//
-//namespace NoisedTests.Commands
-//{
-//    [TestFixture]
-//    public class VolumeCommandTests
-//    {
-//        //[TestFixtureSetUp]
-//        //public void VolumeTestSetUp()
-//        //{
-//        //    IoC.Build();
-//        //}
-//
-//        //[Test]
-//        //public void SetVolumeTest()
-//        //{
-//        //    var serviceConnectionMock = new Mock<IServiceConnectionContext>();
-// 
-//        //    IoC.Get<ICore>().ExecuteCommand(new SetVolume(serviceConnectionMock.Object, 50));
-//
-//        //    IoC.Get<IMediaManager>().Volume.ShouldEqual(50);
-//
-//        //    IoC.Get<ICore>().ExecuteCommand(new SetVolume(serviceConnectionMock.Object, 42));
-//
-//        //    IoC.Get<IMediaManager>().Volume.ShouldEqual(42);
-//
-//        //    IoC.Get<ICore>().ExecuteCommand(new SetVolume(serviceConnectionMock.Object, 101));
-//
-//        //    IoC.Get<IMediaManager>().Volume.ShouldEqual(100);
-//        //}
-//
-//        //[Test]
-//        //public void GetVolumeTest()
-//        //{
-//        //    var responseMetaData = new ResponseMetaData();
-//        //    var serviceConnectionMock = new Mock<IServiceConnectionContext>();
-//        //    serviceConnectionMock.Setup(x => x.SendResponse(It.IsAny<ResponseMetaData>())).Callback((ResponseMetaData r) => responseMetaData = r);
-//
-//        //    new GetVolume(serviceConnectionMock.Object).ExecuteCommand();
-//
-//        //    responseMetaData.Parameters[0].ShouldEqual(50);
-//
-//        //    IoC.Get<IMediaManager>().Volume = 66;
-//
-//        //    new GetVolume(serviceConnectionMock.Object).ExecuteCommand();
-//
-//        //    responseMetaData.Parameters[0].ShouldEqual(66);
-//        //}
-//    }
-//}
+﻿using Moq;
+using NUnit.Framework;
+using Noised.Core.Commands;
+using Noised.Core.Media;
+using Noised.Plugins.Commands.CoreCommands;
+
+namespace NoisedTests.Commands
+{
+    [TestFixture]
+    public class VolumeCommandTests : AbstractCommandTest
+    {
+        [Test]
+        public void SetVolumeTest()
+        {
+            var mediaManager = new Mock<IMediaManager>();
+            RegisterToDIMock<IMediaManager>(mediaManager.Object);
+            new SetVolume(ContextMock.Object, 50).ExecuteCommand();
+            mediaManager.VerifySet(mm => mm.Volume = 50);
+        }
+
+        [Test]
+        public void GetVolumeTest()
+        {
+            var mediaManager = new Mock<IMediaManager>();
+            RegisterToDIMock<IMediaManager>(mediaManager.Object);
+
+            new GetVolume(ContextMock.Object).ExecuteCommand();
+
+            mediaManager.VerifyGet(mm => mm.Volume);
+            string expectedResponseName = "Noised.Plugins.Commands.CoreCommands.GetVolume";
+            ContextMock.Verify(ctx => ctx.SendResponse(
+                        It.Is<ResponseMetaData>(r => r.Name == expectedResponseName)));
+        }
+    }
+}
