81790d23e9d74b92c53d0bf6f4fa5cc147b22450
sebingel <sebingel+github@googlemail.com>
2016-06-02 17:43:37 +0200

    - Edited Unit Tests in MetaFileAccumulatorTests

--- src/NoisedTests/Media/MetaFileAccumulatorTests.cs
+++ src/NoisedTests/Media/MetaFileAccumulatorTests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using FizzWare.NBuilder;
-using FizzWare.NBuilder.Generators;
 using Moq;
 using Noised.Core.DB;
 using Noised.Core.Media;
@@ -103,34 +101,95 @@ namespace NoisedTests.Media
         [Test]
         public void MetaFileAccumulator_Refresh_OneMediaSourceSearchResult_RefreshMetaFiles()
         {
+            // Arrange
+            var testData = new List<Tuple<string, string>>
+            {
+                Tuple.Create("Metallica", "Master of Puppets"),
+                Tuple.Create("Metallica", "Death Magnetic"),
+                Tuple.Create("SuperAwesomeSuperBand", "SuperAwesomeSuperAlbum"),
+                Tuple.Create("System of a Down", "Toxicity"),
+                Tuple.Create("Motörhead", "Bomber"),
+                Tuple.Create("Avenged Sevenfold", "Hail to the King"),
+                Tuple.Create("Foo Fighters", "In your Honor"),
+                Tuple.Create("Led Zeppelin", "Led zeppelin IV"),
+                Tuple.Create("Lillasyster", "Hjärndöd Kärlek"),
+                Tuple.Create("Metallica", "...And justice for all"),
+                Tuple.Create("Sabaton", "Heroes")
+            };
+
+            var metaDataList = new List<MetaData>();
+            foreach (var item in testData)
+            {
+                metaDataList.Add(new MetaData
+                {
+                    Artists = new List<string> { item.Item1 },
+                    Album = item.Item2,
+                    AlbumArtists = new List<string>()
+                });
+            }
+
+            var mediaItemList = new List<MediaItem>();
+            foreach (var item in metaDataList)
+                mediaItemList.Add(new MediaItem(new Uri("file://"), "checksum") { MetaData = item });
+
+            var searchResult = new MediaSourceSearchResult("mockSource", mediaItemList);
+
+            // Mocks
             var mfScraper = new Mock<IMetaFileScraper>();
-            mfScraper.Setup(x => x.GetArtistPictures(It.IsAny<string>())).Returns(new List<MetaFile> { new MetaFile() });
+            mfScraper.Setup(x => x.GetArtistPictures(It.IsAny<string>()))
+                .Returns<string>(
+                    x =>
+                        new List<MetaFile>
+                        {
+                            new MetaFile(x, String.Empty, MetaFileType.ArtistPicture, new Uri("file://"), null, "ext",
+                                MetaFileCategory.Gallery, "orifilename")
+                        });
+            mfScraper.Setup(x => x.GetAlbumCover(It.IsAny<ScraperAlbumInformation>()))
+                .Returns<ScraperAlbumInformation>(
+                    x =>
+                        new List<MetaFile>
+                        {
+                            new MetaFile(x.Artist, x.Album, MetaFileType.ArtistPicture, new Uri("file://"), null, "ext",
+                                MetaFileCategory.Gallery, "orifilename")
+                        });
+
 
             var pL = new Mock<IPluginLoader>();
             pL.Setup(x => x.GetPlugins<IMetaFileScraper>()).Returns(new List<IMetaFileScraper> { mfScraper.Object });
 
+            var uow = new Mock<IUnitOfWork>();
+            uow.Setup(x => x.MetaFileRepository.CreateMetaFile(It.IsAny<MetaFile>())).Callback(() => { });
+            uow.Setup(x => x.SaveChanges()).Callback(() => { });
+
             var dbFac = new Mock<IDbFactory>();
+            dbFac.Setup(x => x.GetUnitOfWork()).Returns(uow.Object);
+
             var mfWriter = new Mock<IMetaFileWriter>();
+            mfWriter.Setup(x => x.WriteMetaFileToDisk(It.IsAny<IMetaFile>())).Callback(() => { });
 
             var msAcc = new Mock<IMediaSourceAccumulator>();
-            var metaDataList =
-                Builder<MetaData>.CreateListOfSize(10)
-                    .All()
-                    .With(x => x.AlbumArtists = Enumerable.Range(0, 1).Select(el => GetRandom.String(10)).ToList())
-                    .And(x => x.Artists = Enumerable.Range(0, 1).Select(el => GetRandom.String(10)).ToList())
-                    .Build();
-            var mediaItemList =
-                Builder<MediaItem>.CreateListOfSize(10)
-                    .All().WithConstructor(() => new MediaItem(new Uri("file://"), "checksum"))
-                    .With(x => x.MetaData = Pick<MetaData>.RandomItemFrom(metaDataList))
-                    .Build();
-            var searchResult =
-                Builder<MediaSourceSearchResult>.CreateNew()
-                    .WithConstructor(() => new MediaSourceSearchResult("mockSource", mediaItemList)).Build();
             msAcc.Setup(x => x.Search(It.IsAny<string>())).Returns(new List<MediaSourceSearchResult> { searchResult });
 
+            // Act
             var mfa = new MetaFileAccumulator(pL.Object, dbFac.Object, mfWriter.Object, msAcc.Object);
             mfa.Refresh();
+
+            // Assert
+            mfScraper.Verify(x => x.GetArtistPictures(It.IsIn(testData.Select(y => y.Item1))), Times.Exactly(9)); // 9 because 9 unique artists
+            mfScraper.Verify(
+                x =>
+                    x.GetAlbumCover(
+                        It.Is<ScraperAlbumInformation>(
+                            y => testData.Any(z => z.Item1 == y.Artist) && testData.Any(z => z.Item2 == y.Album))),
+                Times.Exactly(11)); // 11 because 11 unique albums
+            mfWriter.Verify(
+                x =>
+                    x.WriteMetaFileToDisk(
+                        It.Is<MetaFile>(
+                            y => testData.Any(z => z.Item1 == y.Artist) && testData.Any(z => z.Item2 == y.Album))),
+                Times.AtLeastOnce);
+            uow.Verify(x => x.MetaFileRepository.CreateMetaFile(It.IsAny<MetaFile>()), Times.AtLeastOnce);
+            uow.Verify(x => x.SaveChanges(), Times.AtLeastOnce);
         }
 
         [Test]
