0d0a4271c715e96d35863d54c4c080bca9fff053
sebingel <sebingel+github@googlemail.com>
2016-06-06 13:06:31 +0200

    - added/changed parameter validation in UserManager

--- src/NoisedCore/UserManagement/UserManager.cs
+++ src/NoisedCore/UserManagement/UserManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Noised.Core.Crypto;
 using Noised.Core.DB;
-using PasswordSecurity;
 
 namespace Noised.Core.UserManagement
 {
@@ -20,32 +19,42 @@ namespace Noised.Core.UserManagement
         /// <param name="passwordManager"></param>
         public UserManager(IDbFactory dbFactory, IPasswordManager passwordManager)
         {
+            if (dbFactory == null)
+                throw new ArgumentNullException("dbFactory");
+            if (passwordManager == null)
+                throw new ArgumentNullException("passwordManager");
+
             _dbFactory = dbFactory;
             _passwordManager = passwordManager;
         }
 
         #region IUserManager
-        
+
         public bool Authenticate(string username, string password)
         {
+            if (String.IsNullOrWhiteSpace(username))
+                throw new ArgumentException("username cannot be null or empty", "username");
+            if (String.IsNullOrWhiteSpace(password))
+                throw new ArgumentException("password cannot be null or empty", "password");
+
             User user;
             using (IUnitOfWork iuw = _dbFactory.GetUnitOfWork())
                 user = iuw.UserRepository.GetUser(username);
-        
+
             if (user != null && _passwordManager.VerifyPassword(password, user.PasswordHash))
                 return true;
             return false;
         }
-        
+
         public User CreateUser(string username, string password)
         {
             if (string.IsNullOrWhiteSpace(username))
-                throw new ArgumentException("Value cannot be null or whitespace.", nameof(username));
+                throw new ArgumentException("Value cannot be null or whitespace.", "username");
             if (string.IsNullOrWhiteSpace(password))
-                throw new ArgumentException("Value cannot be null or whitespace.", nameof(password));
+                throw new ArgumentException("Value cannot be null or whitespace.", "password");
 
             string hashedPw = _passwordManager.CreateHash(password);
-        
+
             using (IUnitOfWork iuw = _dbFactory.GetUnitOfWork())
             {
                 var user = new User(username) { PasswordHash = hashedPw };
@@ -54,10 +63,10 @@ namespace Noised.Core.UserManagement
                 return user;
             }
         }
-        
+
         public void DeleteUser(User user)
         {
-            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (user == null) throw new ArgumentNullException("user");
 
             using (IUnitOfWork iuw = _dbFactory.GetUnitOfWork())
             {
@@ -68,7 +77,7 @@ namespace Noised.Core.UserManagement
 
         public void UpdateUser(User user)
         {
-            if (user == null) throw new ArgumentNullException(nameof(user));
+            if (user == null) throw new ArgumentNullException("user");
 
             using (IUnitOfWork iuw = _dbFactory.GetUnitOfWork())
             {
@@ -80,7 +89,7 @@ namespace Noised.Core.UserManagement
         public User GetUser(string username)
         {
             if (string.IsNullOrWhiteSpace(username))
-                throw new ArgumentException("Value cannot be null or whitespace.", nameof(username));
+                throw new ArgumentException("Value cannot be null or whitespace.", "username");
 
             using (IUnitOfWork iuw = _dbFactory.GetUnitOfWork())
                 return iuw.UserRepository.GetUser(username);
